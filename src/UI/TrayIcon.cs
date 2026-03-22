ab24c4f4caefd11b7c0c360e0b28b33cad3507d1
KilimcininKorOglu <229108989+KilimcininKorOglu@users.noreply.github.com>
2026-03-22 23:58:56 +0300

    refactor: remove unused using directives from NotificationService, WindowTracker, and TrayIcon

--- src/UI/TrayIcon.cs
+++ src/UI/TrayIcon.cs
@@ -3,7 +3,6 @@ using System.Drawing;
 using System.IO;
 using System.Reflection;
 using System.Windows.Forms;
-using WindowsNotificationManager.src.Services;
 using WindowsNotificationManager.src.Utils;
 
 namespace WindowsNotificationManager.src.UI
